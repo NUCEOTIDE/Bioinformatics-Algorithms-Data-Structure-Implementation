286f8cca70a7378c067fd2e7df3fa963c28a7965
Jeff Dui <1174980556@qq.com>
2018-08-02 09:08:23 +0800

    Update: major update 1
    BWT algorithm

---
+++ Burrows-Wheeler_Transform/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Burrows_Wheeler homework=new Burrows_Wheeler();
+        homework.calculation();
+    }
+}
